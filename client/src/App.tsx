5198a431ec4b2d9e8cca093804b06633f4aa7412
hoangm960 <hoangm960@gmail.com>
2025-03-01 21:43:15 +0700

    seperate client and server

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+import ColorPicker from "@components/ColorPicker";
+
+function App() {
+    return (
+        <div className="w-dvw h-dvh flex justify-center items-center">
+            <ColorPicker />
+        </div>
+    );
+}
+
+export default App;
