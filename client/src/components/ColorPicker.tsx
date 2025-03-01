4af7a2992174b78930f994a4ba9364bdf4aaac77
hoangm960 <hoangm960@gmail.com>
2025-03-02 00:12:09 +0700

    fix text in color box too blend in

--- client/src/components/ColorPicker.tsx
+++ client/src/components/ColorPicker.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import ColorFul from "@uiw/react-color-colorful";
-import { hexToHsva } from "@uiw/color-convert";
+import { hsvaToHex } from "@uiw/color-convert";
 
 function ColorPicker() {
     const [hsva, setHsva] = useState({ h: 200, s: 43, v: 90, a: 1 });
@@ -8,13 +8,19 @@ function ColorPicker() {
     return (
         <div className="w-1/2 h-1/2 flex flex-col items-center justify-center gap-8">
             <ColorFul
+                color={hsva}
+                onChange={(color) => {
+                    setHsva(color.hsva);
+                }}
                 disableAlpha={true}
             />
             <div
                 className="w-1/3 h-1/3 rounded-2xl flex justify-center items-center text-2xl"
                 style={{ backgroundColor: hsvaToHex(hsva) }}
             >
-                {hsvaToHex(hsva)}
+                <p style={{ color: hsva.v <= 50 ? "white" : "black" }}>
+                    {hsvaToHex(hsva)}
+                </p>
             </div>
         </div>
     );
